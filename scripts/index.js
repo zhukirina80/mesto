bfcae55c142d5e346bf80cad73a624500eebf825
Irina Zhukovskaya <zhukirina80@mail.ru>
2023-08-03 14:18:56 +0300

    feat: добавить новый функционал

--- scripts/index.js
+++ scripts/index.js
@@ -11,52 +11,76 @@ const popupElement = document.querySelector('.popup_type_element');
 const buttonClosePopupElement = popupElement.querySelector(".popup__button-close_element");
 const container = document.querySelector('.elements__cards');
 const cards = document.querySelector('#element-template').content;
-const formElement = document.querySelector('.popup__form_element');
-const titleInput = formElement.querySelector('.popup__input_type_title');
-const linkInput = formElement.querySelector('.popup__input_type_link');
+const formCard = document.querySelector('.popup__form_element');
+const titleInput = formCard.querySelector('.popup__input_type_title');
+const linkInput = formCard.querySelector('.popup__input_type_link');
 const popupImage = document.querySelector('.popup_type_image');
 const buttonClosePopupImage = popupImage.querySelector(".popup__button-close_image");
 const bigImage = popupImage.querySelector(".popup__image");
 const titleBigImage = popupImage.querySelector(".popup__title-image");
+const popupList = Array.from(document.querySelectorAll('.popup'));
+
+function closePopupKeyEsc(evt) {
+  if (evt.keyCode === 27) {
+    const popupOpened = document.querySelector('.popup_opened');
+    if (popupOpened) {
+      closePopup(popupOpened);
+      formCard.reset();
+    };
+  };
+};
+document.addEventListener('keydown', closePopupKeyEsc);
+
+popupList.forEach((popup) => {
+  popup.addEventListener('click', (evt) => {
+    if (evt.target === popup) {
+      const popupOpened = document.querySelector('.popup_opened');
+      if (popupOpened) {
+        closePopup(popupOpened);
+        formCard.reset();
+      };
+    };
+  });
+});
 
 function openPopup(popup) {
   popup.classList.add('popup_opened');
-}
+};
 
 function closePopup(popup) {
   popup.classList.remove('popup_opened');
-}
+};
 
 profileAditButton.addEventListener("click", function() {
   openPopup(popupProfile);
   nameInput.value = profileName.textContent;
   jobInput.value = profileJob.textContent;
-})
+});
 
 function handleFormSubmit(evt) {
     evt.preventDefault();
     profileName.textContent = nameInput.value;
     profileJob.textContent = jobInput.value;
     closePopup(popupProfile);
-}
+};
 
 buttonClosePopupProfile.addEventListener("click", function() {
   closePopup(popupProfile);
-})
+});
 
 formProfile.addEventListener('submit', handleFormSubmit);
 
 profileAddButton.addEventListener("click", function() {
   openPopup(popupElement);
-})
+});
 
 buttonClosePopupElement.addEventListener("click", function() {
   closePopup(popupElement);
-})
+});
 
 buttonClosePopupImage.addEventListener("click", function() {
   closePopup(popupImage);
-})
+});
 
 const createCard = (item) => {
   const card = cards.querySelector('.element').cloneNode(true);
@@ -78,12 +102,12 @@ const createCard = (item) => {
     bigImage.src = item.link;
     titleBigImage.textContent = item.name;
     bigImage.alt = item.name;
-  }
+  };
 
   image.addEventListener("click", function() {
     openImage(item);
     openPopup(popupImage);
-  })
+  });
 
   return(card);
 }
@@ -93,14 +117,14 @@ function handleAddCardFormSubmit(evt) {
   const newCard = createCard({link: linkInput.value, name: titleInput.value});
   container.prepend(newCard);
   closePopup(popupElement);
-  formElement.reset();
-}
+  formCard.reset();
+};
 
-formElement.addEventListener('submit', handleAddCardFormSubmit);
+formCard.addEventListener('submit', handleAddCardFormSubmit);
 
 initialCards.forEach((item) => {
   const card = createCard(item);
   container.append(card);
-})
+});
 
 
---
+++ scripts/validate.js
@@ -0,0 +1,72 @@
+const validationConfig = {
+  formSelector: '.popup__form',
+  inputSelector: '.popup__input',
+  submitButtonSelector: '.popup__button',
+  inactiveButtonClass: 'popup__button_disabled',
+  inputErrorClass: 'popup__input_type_error',
+  errorClass: 'popup__error_visible'
+};
+
+// Функция, которая добавляет класс с ошибкой
+  const showInputError = (formElement, inputElement, validationConfig) => {
+    inputElement.classList.add(validationConfig.inputErrorClass);
+    const spanElement = formElement.querySelector(`.${inputElement.id}-error`);
+    spanElement.textContent = inputElement.validationMessage;
+    spanElement.classList.add(validationConfig.errorClass);
+  }
+
+  // Функция, которая удаляет класс с ошибкой
+  const hideInputError = (formElement, inputElement, validationConfig) => {
+    inputElement.classList.remove(validationConfig.inputErrorClass);
+    const spanElement = formElement.querySelector(`.${inputElement.id}-error`);
+    spanElement.textContent = '';
+    spanElement.classList.remove(validationConfig.errorClass);
+  }
+
+  // Функция, которая проверяет валидность инпута
+  const isValid = (formElement, inputElement) => {
+    if (!inputElement.validity.valid) {
+      showInputError(formElement, inputElement, validationConfig);
+    } else {
+      hideInputError(formElement, inputElement, validationConfig);
+    };
+  };
+
+  // Функция, которая проверяет на валидность все поля инпута
+  const hasInvalidInput = (inputList) => {
+    return inputList.some(inputElement => !inputElement.validity.valid);
+  };
+
+  // Функция, которая меняет сосояние кнопки формы
+  const toggleButtonState = (inputList, buttonElement, validationConfig) => {
+    if (hasInvalidInput(inputList)) {
+      buttonElement.classList.add(validationConfig.inactiveButtonClass);
+      buttonElement.disabled = true;
+    } else {
+      buttonElement.classList.remove(validationConfig.inactiveButtonClass);
+      buttonElement.disabled = false;
+    };
+  };
+
+  // Функция, которая вешает слушатели на инпуты и кнопку формы
+  const setEventListeners = (formElement, validationConfig) => {
+    const inputList = Array.from(formElement.querySelectorAll(validationConfig.inputSelector));
+    const buttonElement = formElement.querySelector(validationConfig.submitButtonSelector);
+
+    toggleButtonState(inputList, buttonElement, validationConfig);
+    inputList.forEach(inputElement => {
+      inputElement.addEventListener('input', () => {
+        isValid(formElement, inputElement);
+        toggleButtonState(inputList, buttonElement, validationConfig);
+      });
+    });
+  };
+
+// Функция для включения валидации на форме
+const enableValidation = () => {
+  const formList = Array.from(document.querySelectorAll(validationConfig.formSelector));
+  formList.forEach(formElement => {
+    setEventListeners(formElement, validationConfig);
+  });
+};
+enableValidation();
